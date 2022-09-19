6d0d3072736ab364a5a404702da5bc4bc19fa8b3
mazarona <mazenfahem@gmail.com>
2022-09-19 20:35:00 +0200

    Fixed time complexity

--- sort/count_sort.cpp
+++ sort/count_sort.cpp
@@ -1,23 +1,23 @@
 #include <vector>
 
 /*
- * Time Complexity: O(max(mx, sz))
+ * Time Complexity: O(n+k)
  */
 
-void count_sort(std::vector<int> &vec){
-  int sz = vec.size();
-  int mx = vec[0];
+void count_sort(std::vector<int> &vec) {
+  int n = vec.size();
+  int k = vec[0];
 
-  for(int i = 0; i < sz; i++) mx = std::max(mx, vec[i]);
+  for (int i = 0; i < n; i++)
+    k = std::max(k, vec[i]);
 
-  std::vector<int> freq(mx + 1, 0);
-  for(int i = 0; i < sz; i++)
+  std::vector<int> freq(k + 1, 0);
+  for (int i = 0; i < n; i++)
     freq[vec[i]]++;
 
   int cnt = 0;
-  for(int i = 0; i < mx + 1; i++){
-    while(freq[i]--)
+  for (int i = 0; i < k + 1; i++) {
+    while (freq[i]--)
       vec[cnt++] = i;
   }
-  
 }
