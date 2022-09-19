ec19d0d4cf5947e18735248b24f05778555d0a0a
mazarona <mazenfahem@gmail.com>
2022-09-19 06:38:03 +0200

    implemented bubbled sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+/*
+* WORST CASE :  O(n^2)
+* BEST CASE  :  O(n)
+*/
+void bubble_sort(std::vector<int> &vec){
+  int n = vec.size();
+  bool is_sorted = true;
+  for(int i = 0; i < n - 1; i++){
+    for(int k = 0; k < n - i - 1; k++){
+      if(vec[k] > vec[k+1] ){
+        std::swap(vec[k], vec[k+1]);
+        is_sorted = false;
+      }
+    }
+    if(is_sorted) break;
+  }
+}
